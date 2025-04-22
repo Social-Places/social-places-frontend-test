4ab92f23594a03c306f115f8c64e0e7fccdb23bb
Matt Henshilwood <matthew@socialplaces.io>
2025-04-22 10:55:44 +0200

    [MH] TailwindCSS

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,37 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+    <div class="flex justify-center">
+        <a href="https://socialplaces.io/" target="_blank">
+            <img alt="SP logo" class="logo h-[150px]" src="/sp-logo.png"/>
+        </a>
+    </div>
+    <h1>Welcome</h1>
+    <div class="w-2/3 items-center m-auto py-5">
+        <p class="my-2">
+            We are Social Places; a local marketing software agency that provides listings, reputation, bookings and social interactions across multiple platforms and channels.
+        </p>
+        <p class="my-2">
+            We are looking for developer talent to join our team. Here you'll find a basic application outfitted in our tech stack but in a much more limited sense. To do the test,
+            you'll find a brief below containing some items to implement and other sorts of tasks to complete.
+        </p>
+    </div>
 </template>
 
 <style scoped>
 .logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+    transition: filter 300ms;
 }
+
 .logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+    filter: drop-shadow(0 0 2em #646cffaa);
 }
+
 .logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+    filter: drop-shadow(0 0 2em #42b883aa);
 }
 </style>
